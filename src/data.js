828bdf2cfc997112de8fdbdf19dd00c6243cf926
colekins <colekins@gmail.com>
2019-08-25 19:13:56 -0400

    Add policy info for Beto

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ const userData = {
             "issues": {
                 "healthcare": "Medicare for All. Supports negotiating drug costs and promoting generics. No limits on abortion.",
                 "environment": "Supports and expands upon the Green New Deal. Supports ban on fracking and shutting down existing nuclear power reactors.",
-                "economy": "Raise taxes on wealthy with new estate taxes. Expand social programs and establish a federal jobs guarantee. Raise the federal minimum wage to $15. Supports universal paid family and medical leave program. Supports a modernized Glass-Steagall which would break up some of the biggest banks. Supports a commission to study reparations.",
+                "economy": "Raise taxes on wealthy with new estate taxes. Expand social programs and establish a federal jobs guarantee. Raise the federal minimum wage to $15/hour. Supports universal paid family and medical leave program. Supports a modernized Glass-Steagall which would break up some of the biggest banks. Supports a commission to study reparations.",
                 "education": "Free tuition at public colleges and universities. Cancellation of all student loan debt. Would work with states to set a minimum salary for teachers of $60,000.",
                 "immigration": "Supports citizenship for dreamers. Would repeal the illegal entry statute which has been used to separate families at the border. Opposed to border wall funding.",
                 "guncontrol": "Supports a voluntary buyback program and universal background checks.",
@@ -21,7 +21,6 @@ const userData = {
                 "military": "Slash the defense budget and bring troops home from post-9/11 wars."
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/men/53.jpg",
                 "icon": "https://pbs.twimg.com/profile_images/1097820307388334080/9ddg5F6v_400x400.png"}
         },
         {
@@ -37,8 +36,8 @@ const userData = {
             },
             "issues": {
                 "healthcare": "Supports Sanders' Medicare for All bill. Supports drug price negotiation, reference pricing and allowing the U.S. government to make generics",
-                "environment": "Supports the Green New Deal and a ban on fracking. Accelerate the transition to clean energy.",
-                "economy": "Raise taxes on wealthy via 'Ultra-Millionaire Tax' to expand social programs like universal childcare. Federal funds to build and perserve more affordable housing. Raise the federal minimum wage to $15. Supports universal paid family and medical leave program.  Supports a modernized Glass-Steagall which would break up some of the biggest banks. Supports a commission to study reparations.",
+                "environment": "Supports the Green New Deal and a ban on fracking. Plans to accelerate the transition to clean energy.",
+                "economy": "Raise taxes on wealthy via 'Ultra-Millionaire Tax' to expand social programs like universal childcare. Federal funds to build and perserve more affordable housing. Raise the federal minimum wage to $15/hour. Supports universal paid family and medical leave program.  Supports a modernized Glass-Steagall which would break up some of the biggest banks. Supports a commission to study reparations.",
                 "education": "Free tuition at public colleges and universities. Student loan forgiveness for low and middle-income borrowers.",
                 "immigration": "Supports citizenship for dreamers. Would repeal the illegal entry statute which has been used to separate families at the border. Opposed to border wall funding.",
                 "guncontrol": "Supports a ban on assualt weapons and universal background checks.",
@@ -46,7 +45,6 @@ const userData = {
                 "military": "Slash the defense budget and bring troops home from post-9/11 wars."
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/men/21.jpg",
                 "icon": "http://stopthecap.com/wp-content/uploads/2015/10/warren.jpg"}
         },
         {
@@ -61,18 +59,17 @@ const userData = {
                 "state": "delaware"
             },
             "issues": {
-                "healthcare": "",
-                "environment": "",
-                "economy": "",
-                "education": "",
-                "immigration": "",
-                "guncontrol": "",
-                "criminaljustice": "",
-                "military": ""
+                "healthcare": "Opposes Medicare for All, but would expand coverage, building on the Affordable Care Act. Support negotiating for Medicare drug prices and international reference pricing.",
+                "environment": "Supports the Green New Deal. Supports developing new nuclear technologies to fight climate change.",
+                "economy": "Raise the federal minimum wage to $15/hour. Supports a commission to study reparations but has not explicitly supported one of the existing bills.",
+                "education": "Free two-year community college. Expand or fix existing debt-relief programs. Would Increase Title I funding to give educators more competitive salaries.",
+                "immigration": "Supports citizenship for dreamers.",
+                "guncontrol": "Supports a voluntary buyback program and universal background checks.",
+                "criminaljustice": "Abolish the death penalty and end the cash bail system. Eliminate private prisons and mandatory minimum sentences for nonviolent crimes.",
+                "military": "Increase defense spending and keep the troops deployed."
 
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://miro.medium.com/fit/c/256/256/1*12QebBEwpcHMg_xuaJ5MrQ.jpeg",            },
         },
         {
@@ -87,17 +84,16 @@ const userData = {
                 "state": "Texas"
             },
             "issues": {
-                "healthcare": "",
-                "environment": "",
-                "economy": "",
-                "education": "",
-                "immigration": "",
-                "guncontrol": "",
-                "criminaljustice": "",
-                "military": ""
+                "healthcare": "Opposes Medicare for All, but would expand coverage. Would provide a public option in addition to exisintg private market. Supports negotiating drug prices for Medicare’s outpatient prescription drug benefit",
+                "environment": "Supports the Green New Deal although his own plan is less ambitious.",
+                "economy": "Raise the federal minimum wage to $15/hour. Supports a commission to study reparations.",
+                "education": "Expand or fix existing debt-relief programs. $500 billion fund that states could use for teacher pay and funding gaps. The fund would be financed with a tax on Wall Street speculation.",
+                "immigration": "Supports citizenship for dreamers. Would leave illegal entry statute in place, but has declared a need for broad immigration law reform. Open to tearing down parts of the border wall.",
+                "guncontrol": "Supports a mandatory buyback program for assault weapons and universal background checks. Open to requiring gun licenses.",
+                "criminaljustice": "Abolish the death penalty and end the cash bail system. Eliminate mandatory minimum sentences for nonviolent crimes.",
+                "military": "Increase defense spending, and bring troops home from post-9/11 wars."
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://assets3.thrillist.com/v1/image/2814565/size/sk-2017_04_featured_listing_mobile.jpg"},
         },
         {
@@ -116,7 +112,6 @@ const userData = {
                 "age": 2
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://miro.medium.com/fit/c/256/256/1*otydHRTFB1BbHZ4Xu4dLQA.png",},
         },
         {
@@ -141,7 +136,6 @@ const userData = {
                 "military": ""
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://styles.redditmedia.com/t5_nahld/styles/communityIcon_hxeyojcanp731.jpg"},
         },
         {
@@ -171,7 +165,6 @@ const userData = {
                 "military": ""
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://pbs.twimg.com/profile_images/1091308075041079297/Yz_PLR20.jpg"},
         },
         {
@@ -196,7 +189,6 @@ const userData = {
                 "military": ""
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://pbs.twimg.com/profile_images/1059812997982511105/lgFAlE5t.jpg"},
         },
         {
@@ -215,14 +207,20 @@ const userData = {
                 "age": 2
             },
             "picture": {
-                "large": "https://randomuser.me/api/portraits/women/66.jpg",
                 "icon": "https://miro.medium.com/fit/c/256/256/0*Yu171xeFxAqDw5lx.jpeg"},
         }
     ]
 }
 
-const modifiedData = userData.results.map(function (User) {
-    return User;
+function alphaCompare(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+  
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+  }
+
+const sortedData = userData.results.sort(function(a, b) {
+    return alphaCompare(a.name.last, b.name.last);
 });
 
-export default modifiedData;
+export default sortedData;
