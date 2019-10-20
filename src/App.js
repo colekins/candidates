5d87593577bd40fab9b388f68a674471143a3727
colekins <colekins@gmail.com>
2019-10-20 13:57:43 -0400

    Begin adding theme functionality. Update App state w current theme

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import Header from './common/Header.js';
 import Footer from './common/Footer';
 
 
-function App()  {    
-      return (
-        <div className="App">
-            <Header/>
-            <CandidatesApp />
-            <Footer />
-        </div>
-    );
+class App extends React.Component  {    
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          darkTheme: true
+        };
+    }
+
+    onSwitchTheme = () => {
+        let previous = this.state.darkTheme;
+        this.setState({
+            darkTheme: !previous
+            });
+        console.log(previous)
+        };
+
+    render () {
+        return (
+            <div className="App">
+                <Header darkTheme = {this.state.darkTheme} onSwitchTheme = {this.onSwitchTheme} />
+                <CandidatesApp />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
