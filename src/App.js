f88a9c99cae8dda01c1edbc560102286c8f9a723
colekins <colekins@gmail.com>
2019-10-20 14:33:04 -0400

    Functioning theme switcher. Light theme needs cleanup

--- src/App.js
+++ src/App.js
@@ -15,11 +15,22 @@ class App extends React.Component  {
     }
 
     onSwitchTheme = () => {
-        let previous = this.state.darkTheme;
+        let previouslyDark = this.state.darkTheme;
+
+        if (previouslyDark) {
+            var sheet = document.createElement('style')
+            sheet.innerHTML = "body {background-color: white; color: black;} .candidate-card {background: rgb(238, 238, 238)} .candidate-card:hover {background: linear-gradient(rgb(249, 249, 249) , rgb(240, 240, 240))} .candidate-card-selected {background: linear-gradient(to bottom, rgb(109, 185, 255) , rgb(26, 124, 216))} .header-splash {color: #0076c4} .header-subtitle {color: black;}";
+            document.body.appendChild(sheet);
+        } else 
+        {
+            var lightStyle = document.getElementsByTagName('style')[8];
+            var parentNode = lightStyle.parentNode;
+            parentNode.removeChild(lightStyle);
+        }
+
         this.setState({
-            darkTheme: !previous
+            darkTheme: !previouslyDark
             });
-        console.log(previous)
         };
 
     render () {
